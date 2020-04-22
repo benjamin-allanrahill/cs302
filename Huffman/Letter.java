a8c1f6c6a73749e203f79188652412231e2017de
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-22 08:03:26 -0400

    added encoding object

--- Huffman/Letter.java
+++ Huffman/Letter.java
@@ -3,18 +3,24 @@
  */
 public class Letter {
  
-    private Character _char;
-    private Double weight;
+    Character _char;
+    Double weight;
+    String enconding;
  
     public Letter(Character c, Double weight) {
         this._char = c;
         this.weight = weight;
+        this.enconding = "";
     }
  
     //Getters and setters
 
     public Double getWeight() {
         return this.weight;
+    }
+
+    public String getCode() {
+        return this.enconding;
     }
  
     @Override
