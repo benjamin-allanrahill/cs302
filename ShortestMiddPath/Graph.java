eb6514636adcc010b9b11574685ead2808d9cd50
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-06 21:33:03 -0400

    documentation

--- ShortestMiddPath/Graph.java
+++ ShortestMiddPath/Graph.java
@@ -1,11 +1,15 @@
 package ShortestMiddPath;
 
-/*
-* Author: Shelby Kimmel
-* Creates a adjacency list object to store information about the graph of roads, and contains the main functions used to 
-* run the Bellman Ford algorithm 
-
-*/
+/**
+ * 
+ * 
+ * Author: Shelby Kimmel
+ * Creates a adjacency list object to store information about the graph of roads, and contains the main functions used to 
+ * run the Bellman Ford algorithm 
+ * Co-Author: Benjamin Allan-Rahill
+ * Time spent: 2 hours
+ * 
+ */
 
 import java.io.File;
 import java.util.Scanner;
@@ -77,45 +81,39 @@ public class Graph {
 	//Based on https://stackoverflow.com/questions/12134687/how-to-add-element-into-arraylist-in-hashmap 
 	public synchronized void addToList(Road road) {
 		Integer node = road.getStart();
-    	ArrayList<Road> roadList = this.getAdjList().get(node);
+		ArrayList<Road> roadList = this.getAdjList().get(node);
 
-    	// if node is not already in adjacency list, we creat a list for it
-    	if(roadList == null) {
-    	    roadList = new ArrayList<Road>();
-    	    roadList.add(road);
-   		    this.getAdjList().put(node, roadList);
-  	  	} 
-  	  	else {
-        	// add if item is not already in list
-        	if(!roadList.contains(road)) roadList.add(road);
-    	}
-    	
-    }
+		// if node is not already in adjacency list, we creat a list for it
+		if (roadList == null) {
+			roadList = new ArrayList<Road>();
+			roadList.add(road);
+			this.getAdjList().put(node, roadList);
+		} else {
+			// add if item is not already in list
+			if (!roadList.contains(road))
+				roadList.add(road);
+		}
 
-    public Double[][] ShortestDistance(Integer startNode){
-    	// This method should create the array storing the objective function values of subproblems used in Bellman Ford.
+	}
 
-		//You need to write this method!!
+	/**
+	 * This method returns the array with lists of shortest distances 
+	 * @param startNode the node to start the path from 
+	 * @return Double[][] Shortest Distance Array
+	 */
 
-		
+	public Double[][] ShortestDistance(Integer startNode) {
+		// This method should create the array storing the objective function values of subproblems used in Bellman Ford.
 
-		// this.adjList.entrySet().forEach(entry -> {
-		// 	System.out.println(entry.getKey() + " : " + entry.getValue());
-		// });
-		// this.nodeDict.entrySet().forEach(entry -> {
-		// 	System.out.println(entry.getKey() + " : " + entry.getValue());
-		// });
-		// System.out.println(nodeDict.size());
-		// System.out.println(nodeDict.get(startNode));
-		
-		
 		Double[][] dpArray = new Double[nodeDict.size()][nodeDict.size()];//You should probably do a different initialization here
-		
+
+		// initalize array with infinity and zero
 		for (int i = 0; i < dpArray.length; i++) {
 			dpArray[i][0] = Double.MAX_VALUE;
 		}
 		dpArray[this.nodeDict.get(startNode)][0] = 0.0;
 
+		// fill up array w/ shortest paths
 		for (int i = 1; i < this.nodeDict.size(); i++) {
 			for (Integer node : this.nodeDict.keySet()) {
 				int v = this.nodeDict.get(node);
@@ -124,23 +122,31 @@ public class Graph {
 					if ((dpArray[this.nodeDict.get(road.endNode)][i - 1] + road.miles) < dpArray[v][i]) {
 						dpArray[v][i] = dpArray[this.nodeDict.get(road.endNode)][i - 1] + road.miles;
 					}
-					
-				};
+
+				}
+				;
 			}
 		}
 
-		
 		return dpArray;
-    }
-
+	}
+	
+	/**
+	 * Works backwards through array and calculates the path to be taken; prints the path.
+	 * @param endNode the location where the path should end
+	 * @param dpArray the array of the shortest distances 
+	 */
     public void ShortestPath(Integer endNode, Double[][] dpArray){
 		// This method should work backwards through the array you created in ShortestDistance and output the 
 		// sequence of streets you should take to get from your starting point to your ending point.
 
+		// initalize values
 		int i = this.nodeDict.size() - 1;
 		int v = endNode;
 		ArrayList<String> streetList = new ArrayList<>();
 
+
+		// work "backwards" through the array 
 		while (i > 0) {
 			System.out.println(dpArray[this.nodeDict.get(v)][i]);
 			if (dpArray[this.nodeDict.get(v)][i] != dpArray[this.nodeDict.get(v)][i - i]) {
@@ -156,10 +162,8 @@ public class Graph {
 			i--;
 		}
 
+		// print the path
 		System.out.println(streetList);
-
-
-		System.out.println("You need to write some code!");
 	}			
 				
 
